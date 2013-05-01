f7cdf3082bf280d2c78c5b2e2f4ab38f033b64d1
João Colombo <joaocolombo@outlook.com>
2013-05-01 06:50:45 -0300

    Fixed keyboard shortcuts view window.

--- Textdown/docs/keyboard_shortcuts.js
+++ Textdown/docs/keyboard_shortcuts.js
@@ -1,3 +1,5 @@
-window.onload = function() {
-	window.document.body.innerHTML = window.document.body.innerHTML.replace(/Command/gi, "Control");
-};
+if (window.navigator.platform.toLowerCase().indexOf("mac") < 0) {
+	window.onload = function() {
+		window.document.body.innerHTML = window.document.body.innerHTML.replace(/Command/gi, "Control");
+	};
+}
